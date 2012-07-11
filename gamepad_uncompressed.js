bbb97f2291b59f6a0b0a0ecebe01d586f8aae861
Justin Walsh <contact.me@thejustinwalsh.com>
2012-07-11 10:19:37 -0700

    Generated src from the build script, including the latest changes

--- gamepad_uncompressed.js
+++ gamepad_uncompressed.js
@@ -99,8 +99,8 @@
             }
         }
         mapped.name = "Unknown: " + raw.id;
-        //console.warn("Unrecognized pad type, not being mapped!");
-        //console.warn(raw.id);
+        console.warn("Unrecognized pad type, not being mapped!");
+        console.warn(raw.id);
     };
 
     var mapIndividualPad = function(rawPads, i) {
@@ -238,10 +238,49 @@
 
     var ChromeMacXbox360Controller = function(raw, into, index) {
         CommonMacXbox360Controller(raw, into, index);
-        into.rightStickX = raw.axes[3];
-        into.rightStickY = raw.axes[4];
-        into.leftShoulder1 = axisToButton(raw.axes[2]);
+        into.rightStickX = raw.axes[2];
+        into.rightStickY = raw.axes[3];
+        into.leftShoulder1 = raw.buttons[6];
+        into.rightShoulder1 = raw.buttons[7];
+        into.leftStickButton = raw.buttons[10];
+        into.rightStickButton = raw.buttons[11];
+        into.select = raw.buttons[8];
+        into.start = raw.buttons[9];
+        into.dpadUp = raw.buttons[12];
+        into.dpadDown = raw.buttons[13];
+        into.dpadLeft = raw.buttons[14];
+        into.dpadRight = raw.buttons[15];
+    };
+
+    var FirefoxMacXbox360Controller = function(raw, into, index) {
+        CommonMacXbox360Controller(raw, into, index);
+        into.rightStickX = raw.axes[2];
+        into.rightStickY = raw.axes[3];
+        into.leftShoulder1 = axisToButton(raw.axes[4]);
         into.rightShoulder1 = axisToButton(raw.axes[5]);
+    };
+
+    var CommonMacPS3Controller = function(raw, into, index) {
+        // NOTE: Partial, doesn't set all values.
+        into.leftStickX = raw.axes[0];
+        into.leftStickY = raw.axes[1];
+        into.rightStickX = raw.axes[2];
+        into.faceButton0 = raw.buttons[14];
+        into.faceButton1 = raw.buttons[13];
+        into.faceButton2 = raw.buttons[15];
+        into.faceButton3 = raw.buttons[12];
+        into.leftShoulder0 = raw.buttons[10];
+        into.rightShoulder0 = raw.buttons[11];
+        into.leftShoulder1 = raw.buttons[8];
+        into.rightShoulder1 = raw.buttons[9];
+        into.select = raw.buttons[0];
+        into.start = raw.buttons[3];
+        into.leftStickButton = raw.buttons[1];
+        into.rightStickButton = raw.buttons[2];
+        into.dpadUp = raw.buttons[4];
+        into.dpadDown = raw.buttons[6];
+        into.dpadLeft = raw.buttons[7];
+        into.dpadRight = raw.buttons[5];
     };
 
     var ChromeMacLogitechF310Controller = function(raw, into, index) {
@@ -275,37 +314,6 @@
         into.rightStickY = raw.axes[5];
         into.leftShoulder1 = raw.buttons[6];
         into.rightShoulder1 = raw.buttons[7];
-    };
-
-    var FirefoxMacXbox360Controller = function(raw, into, index) {
-        CommonMacXbox360Controller(raw, into, index);
-        into.rightStickX = raw.axes[2];
-        into.rightStickY = raw.axes[3];
-        into.leftShoulder1 = axisToButton(raw.axes[4]);
-        into.rightShoulder1 = axisToButton(raw.axes[5]);
-    };
-
-    var CommonMacPS3Controller = function(Raw, into, index) {
-        // NOTE: Partial, doesn't set all values.
-        into.leftStickX = raw.axes[0];
-        into.leftStickY = raw.axes[1];
-        into.rightStickX = raw.axes[2];
-        into.faceButton0 = raw.buttons[14];
-        into.faceButton1 = raw.buttons[13];
-        into.faceButton2 = raw.buttons[15];
-        into.faceButton3 = raw.buttons[12];
-        into.leftShoulder0 = raw.buttons[10];
-        into.rightShoulder0 = raw.buttons[11];
-        into.leftShoulder1 = raw.buttons[8];
-        into.rightShoulder1 = raw.buttons[9];
-        into.select = raw.buttons[0];
-        into.start = raw.buttons[3];
-        into.leftStickButton = raw.buttons[1];
-        into.rightStickButton = raw.buttons[2];
-        into.dpadUp = raw.buttons[4];
-        into.dpadDown = raw.buttons[6];
-        into.dpadLeft = raw.buttons[7];
-        into.dpadRight = raw.buttons[5];
     };
 
     var FirefoxMacPS3Controller = function(raw, into, index) {
